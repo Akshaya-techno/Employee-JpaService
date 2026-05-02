c7412de972a7a6c2fdc48753b72990aff65f55ff
Akshaya-techno <akshayatechnochan01@gmail.com>
2026-05-02 11:11:55 +0000

    first commit in employee jpaservice

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -7,3 +7,52 @@
  */
 
 // Write your code here
+/*
+ * You can use the following import statements
+ *
+ * import org.springframework.beans.factory.annotation.Autowired;
+ * import org.springframework.web.bind.annotation.*;
+ * import java.util.*;
+ */
+
+// Write your code here
+package com.example.employee.controller;
+
+import com.example.employee.service.EmployeeH2Service;
+import com.example.employee.model.*;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.*;
+
+@RestController
+public class EmployeeController {
+    @Autowired
+    private EmployeeH2Service employeeH2Service;
+
+    @GetMapping("/employees")
+    public List<Employee> getAllEmployee() {
+        return employeeH2Service.getAllEmployee();
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        return employeeH2Service.addEmployee(employee);
+    }
+
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployeeById(@PathVariable("employeeId") int employeeId) {
+        return employeeH2Service.getEmployeeById(employeeId);
+    }
+
+    @PutMapping("/employees/{employeeId}")
+    public Employee updateEmployee(@PathVariable("employeeId") int employeeId, @RequestBody Employee employee) {
+        return employeeH2Service.updateEmployee(employeeId, employee);
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public void deleteEmployee(@PathVariable("employeeId") int employeeId) {
+        employeeH2Service.deleteEmployee(employeeId);
+    }
+
+}
--- src/main/java/com/example/employee/repository/EmployeeRepository.java
+++ src/main/java/com/example/employee/repository/EmployeeRepository.java
@@ -1,1 +1,18 @@
 // Write your code here
+package com.example.employee.repository;
+
+import com.example.employee.model.Employee;
+import java.util.*;
+
+public interface EmployeeRepository {
+    List<Employee> getAllEmployee();
+
+    Employee addEmployee(Employee employee);
+
+    Employee getEmployeeById(int employeeId);
+
+    Employee updateEmployee(int employeeId, Employee employee);
+
+    void deleteEmployee(int employeeId);
+
+}
